6f76af740d6bc9adb50076ea3a9c2afb17944ede
Sven Kreiss <sk@svenkreiss.com>
2014-05-30 23:27:41 -0400

    Also serve non-local requests.

--- databench/app.py
+++ databench/app.py
@@ -45,7 +45,7 @@ def index():
 
 def run():
 	port = int(os.environ.get('PORT', 5000))
-	socketio.run(flaskapp, port=port)
+	socketio.run(flaskapp, host='0.0.0.0', port=port)
 	
 
 if __name__ == '__main__':
