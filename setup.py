d22439b749ed086e7fbd38424190f4c391518704
Sven Kreiss <sk@svenkreiss.com>
2014-05-30 22:11:26 -0400

    Include static and templates in pip package. Version 0.1.0.

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from distutils.core import setup
 
 setup(
     name='databench',
-    version='0.0.4',
+    version='0.1.0',
     packages=['databench', 'scripts', 'analyses_packaged'],
     license='LICENSE',
     description='Data analysis tool using Flask, SocketIO and d3.js.',
     long_description=open('README.md').read(),
     author='Sven Kreiss',
     author_email='sk@svenkreiss.com',
+
+    include_package_data = True,
 
     install_requires=[
         'Flask>=0.10.1',
@@ -28,20 +30,5 @@ setup(
         'console_scripts': [
             'databench = scripts.exec:main',
         ]
-    },
-
-    data_files=[
-        ('databench/static/', ['databench.js', 'favicon.ico', 'main.css']),
-
-        ('databench/static/bootstrap-3.1.1-dist/css', ['bootstrap.css','bootstrap.min.css','bootstrap-theme.css','bootstrap-theme.min.css']),
-        ('databench/static/bootstrap-3.1.1-dist/js', ['bootstrap.js','bootstrap.min.js']),
-        ('databench/static/bootstrap-3.1.1-dist/fonts', ['glyphicons-halflings-regular.eot','glyphicons-halflings-regular.svg','glyphicons-halflings-regular.ttf','glyphicons-halflings-regular.woff']),
-
-        ('databench/static/d3', ['d3.v3.min.js']),
-        ('databench/static/jquery', ['jquery-2.1.1.min.js']),
-        ('databench/static/mpld3', ['mpld3.v0.2.js']),
-        ('databench/static/socket.io', ['socket.io.min.js']),
-
-        ('databench/templates', ['base.html','index.html']),
-    ]
+    }
 )
