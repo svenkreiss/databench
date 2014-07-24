dc83e43ab828e2a4468fddecc0ae3a98273b6b3b
Sven Kreiss <sk@svenkreiss.com>
2014-07-24 14:53:36 -0400

    Update style.

--- analyses_packaged/dummypi.py
+++ analyses_packaged/dummypi.py
@@ -8,11 +8,11 @@ from random import random
 import databench
 
 
-dummypi = databench.Analysis('dummypi', __name__)
-dummypi.thumbnail = 'dummypi.png'
-dummypi.description = __doc__
+ANALYSIS = databench.Analysis('dummypi', __name__, __doc__)
+ANALYSIS.thumbnail = 'dummypi.png'
 
-@dummypi.signals.on('connect')
+
+@ANALYSIS.signals.on('connect')
 def onconnect():
     """Run as soon as a browser connects to this."""
 
@@ -23,20 +23,20 @@ def onconnect():
         if r1*r1 + r2*r2 < 1.0:
             inside += 1
 
-        if (i+1)%100 == 0:
+        if (i+1) % 100 == 0:
             draws = i+1
-            dummypi.signals.emit('log', {
-                'draws':draws,
-                'inside':inside,
-                'r1':r1,
-                'r2':r2,
+            ANALYSIS.signals.emit('log', {
+                'draws': draws,
+                'inside': inside,
+                'r1': r1,
+                'r2': r2,
             })
 
             p = float(inside)/draws
             uncertainty = 4.0*math.sqrt(draws*p*(1.0 - p)) / draws
-            dummypi.signals.emit('status', {
+            ANALYSIS.signals.emit('status', {
                 'pi-estimate': 4.0*inside/draws,
                 'pi-uncertainty': uncertainty
             })
 
-    dummypi.signals.emit('log', {'action': 'done'})
+    ANALYSIS.signals.emit('log', {'action': 'done'})
