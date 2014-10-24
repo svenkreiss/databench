b410dd3afd4ffcec7815eea1737f732beb16997a
Sven Kreiss <sk@svenkreiss.com>
2014-10-24 12:15:00 +0200

    expose databench_py.singlethread

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ else:
 setup(
     name='databench',
     version=VERSION,
-    packages=['databench', 'databench_py'],
+    packages=['databench', 'databench_py', 'databench_py.singlethread'],
     license='MIT',
     description='Data analysis tool using Flask, WebSockets and d3.js.',
     long_description=open('README.rst').read(),
