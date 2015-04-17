e0c5e17532eddba3da42d7a7f9971e7392ed4f5f
Sven Kreiss <me@svenkreiss.com>
2015-04-17 15:27:27 -0400

    new version 0.3.15 to update pypi docs

--- databench/__init__.py
+++ databench/__init__.py
@@ -1,6 +1,6 @@
 """Databench module."""
 
-__version__ = "0.3.14"
+__version__ = "0.3.15"
 
 # Need to make sure monkey.patch_all() is applied before any
 # 'import threading', but cannot raise error because building the Sphinx
