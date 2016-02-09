f1ed3822f4300a19bd3d6787d036e830ca8db5d3
Sven Kreiss <me@svenkreiss.com>
2016-02-09 12:49:52 -0500

    datastore test cases for list and dict. cleanup.

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -1,26 +1,34 @@
 import databench
 
+
 data_after = None
 
 
+def cb(key, value):
+    global data_after
+    print('{} changed to {}'.format(key, value))
+    data_after = value
+
+
+d = databench.Datastore('abcdef').on_change(cb)
+
+
 def test_datastore():
-    def cb(key, value):
-        global data_after
-        print('{} changed to {}'.format(key, value))
-        data_after = value
-
-    d = databench.Datastore('abcdef').on_change(cb)
     d['test'] = 'trivial'
-
     assert data_after == 'trivial'
 
 
-def test_analysis_datastore():
-    def cb(key, value):
-        global data_after
-        print('{} changed to {}'.format(key, value))
-        data_after = value
+def test_datastore_list():
+    d['test'] = ['list']
+    assert data_after == ['list']
 
+
+def test_datastore_dict():
+    d['test'] = {'key': 'value'}
+    assert data_after == {'key': 'value'}
+
+
+def test_analysis_datastore():
     a = databench.Analysis()
     a.set_emit_fn(lambda s, pl: None)
     a.data.on_change(cb)
@@ -32,4 +40,6 @@ def test_analysis_datastore():
 
 if __name__ == '__main__':
     test_datastore()
+    test_datastore_list()
+    test_datastore_dict()
     test_analysis_datastore()
