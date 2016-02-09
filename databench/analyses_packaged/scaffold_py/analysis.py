3c5bef1434c05e60b438ee4cefaea912e230ac62
Sven Kreiss <me@svenkreiss.com>
2016-02-09 10:01:05 -0500

    simplify scaffold

--- databench/analyses_packaged/scaffold_py/analysis.py
+++ databench/analyses_packaged/scaffold_py/analysis.py
@@ -8,23 +8,17 @@ import databench_py.singlethread
 class Analysis(databench_py.Analysis):
 
     def on_connect(self):
-        """Run as soon as a browser connects to this.
-
-        CHANGEME
-        """
+        """Run as soon as a browser connects to this."""
         self.emit('log', 'backend is connected and initialized')
-        time.sleep(3)
+        time.sleep(2)
         self.emit('ready', 'ready at ' +
                   datetime.datetime.now().isoformat())
 
     def on_got_ready_signal(self, msg):
         """This is the signal the frontend sends back to the backend once
         it received the 'ready' signal from the on_connect() function.
-
-        CHANGEME
         """
-
-        time.sleep(3)
+        time.sleep(2)
         self.emit('log', 'Backend received the confirmation from the frontend '
                   'that the ready signal was received.')
 
