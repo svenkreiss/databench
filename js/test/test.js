d9536dc7963ed25d00843370ecacdd8ce1e295fd
Sven Kreiss <me@svenkreiss.com>
2016-02-15 13:34:54 -0500

    first real mocha test

--- js/test/test.js
+++ js/test/test.js
@@ -5,13 +5,23 @@ var Databench = require('./../node/main');
 
 describe('Databench', function() {
   describe('#Connection', function () {
-    it('create a WebSocket connection', function () {
-      let c = new Databench.Connection(
-        (msg) => console.log(msg),
-        null,
-        'http://localhost:5000/dummypi/ws'
-      );
-      assert.equal('object', typeof c);
+    // create connection
+    let c = new Databench.Connection(
+      (msg) => console.log(msg),
+      null,
+      'http://localhost:5000/dummypi/ws'
+    );
+    it('create a WebSocket connection', function (done) {
+      this.timeout(5000);
+
+      let d;
+      c.on('test_fn', (data) => { d = data; });
+      c.emit('test_fn', [1, 2]);
+
+      setTimeout(() => {
+        assert.deepEqual({first_param: 1, second_param: 2}, d);
+        done();
+      }, 100);
     });
   });
 });
