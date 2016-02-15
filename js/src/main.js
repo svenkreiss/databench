e8c7e196193f63e4540e7bae22a0a066276f3cc6
Sven Kreiss <me@svenkreiss.com>
2016-02-15 12:43:00 -0500

    cleanup

--- js/src/main.js
+++ js/src/main.js
@@ -2,12 +2,7 @@ import * as ui from './ui';
 import { Connection } from './connection';
 
 // create a public interface
-// var Databench04 = {};
-// Databench04.ui = ui;
-// Databench04.Connection = Connection;
-
 if (typeof window !== 'undefined') {
 	window.Databench04 = { ui, Connection };
 }
-
 export { ui, Connection };
