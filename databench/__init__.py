baca5b596f4233fd14907eb08ecfcd1bb8240126
Sven Kreiss <me@svenkreiss.com>
2016-02-22 09:58:44 -0500

    try to fix missing analyses_packaged

--- databench/__init__.py
+++ databench/__init__.py
@@ -1,8 +1,12 @@
 """Databench module."""
 
+from __future__ import absolute_import
+
 __version__ = "0.4.0a"
 
 from .analysis import Meta, Analysis
 from .analysis_zmq import MetaZMQ, AnalysisZMQ
 from .datastore import Datastore
 from .readme import Readme
+
+from . import analyses_packaged
