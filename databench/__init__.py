a08c28acfd11cf17198ae6ea1a84b8c093bd1c51
Sven Kreiss <me@svenkreiss.com>
2016-04-30 11:20:42 -0400

    lint setup: flake8, hacking

--- databench/__init__.py
+++ databench/__init__.py
@@ -2,9 +2,9 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.4a1"
-
 from .analysis import Meta, Analysis
 from .analysis_zmq import MetaZMQ, AnalysisZMQ
 from .datastore import Datastore
 from .readme import Readme
+
+__version__ = "0.4a1"
