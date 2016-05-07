95c883992844a20a11f73aa4954f3350d47f9862
Sven Kreiss <me@svenkreiss.com>
2016-05-06 22:26:37 -0400

    datastore coverage to 100%

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -18,14 +18,39 @@ def test_datastore():
     assert data_after == 'trivial'
 
 
-def test_datastore_list():
+def test_init():
+    d['test'] = 'before-init'
+    d.init({'unset_test': 'init'})
+    assert data_after == 'before-init'
+
+
+def test_update():
+    d.update({'test': 'update'})
+    assert data_after == 'update'
+
+
+def test_set_skip_callback():
+    global data_after
+    d['test'] = 'before-set'
+    data_after = 'skip-callback'
+    d['test'] = 'before-set'
+    assert data_after == 'skip-callback'
+
+
+def test_list():
     d['test'] = ['list']
     assert data_after == ['list']
 
 
-def test_datastore_dict():
+def test_dict():
     d['test'] = {'key': 'value'}
     assert data_after == {'key': 'value'}
+
+
+def test_contains():
+    d['test'] = 'contains'
+    assert 'test' in d
+    assert 'never-user-test' not in d
 
 
 def test_analysis_datastore():
@@ -40,6 +65,9 @@ def test_analysis_datastore():
 
 if __name__ == '__main__':
     test_datastore()
-    test_datastore_list()
-    test_datastore_dict()
+    test_init()
+    test_update()
+    test_list()
+    test_dict()
+    test_contains()
     test_analysis_datastore()
