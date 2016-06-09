cfda2259d891d18425fd1ce6a920c84dc272ec6c
Sven Kreiss <me@svenkreiss.com>
2016-06-09 08:22:31 -0400

    version 0.4a11

--- databench/__init__.py
+++ databench/__init__.py
@@ -3,7 +3,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.4a10"
+__version__ = "0.4a11"
 
 from .analysis import Meta, Analysis, sanitize_message
 from .analysis_zmq import MetaZMQ, AnalysisZMQ
