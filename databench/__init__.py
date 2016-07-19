2b16813cb5a825a7f403034a6790bb5189f91c1a
Sven Kreiss <me@svenkreiss.com>
2016-07-19 10:30:03 -0400

    version 0.4a19

--- databench/__init__.py
+++ databench/__init__.py
@@ -3,7 +3,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.4a18"
+__version__ = "0.4a19"
 
 from .analysis import Meta, Analysis, sanitize_message
 from .analysis_zmq import MetaZMQ, AnalysisZMQ
