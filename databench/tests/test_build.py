c4804e1a41f1d100219b18422ab12afc0cd9ab23
Sven Kreiss <me@svenkreiss.com>
2017-06-30 22:32:51 -0400

    make test independent of install location

--- databench/tests/test_build.py
+++ databench/tests/test_build.py
@@ -1,29 +1,37 @@
+import databench.tests.analyses
+import databench.tests.analyses_broken
 import os
 import subprocess
 import time
 import unittest
 
 
+ANALYSES_PATH = os.path.abspath(
+    os.path.dirname(databench.tests.analyses.__file__))
+ANALYSES_BROKEN_PATH = os.path.abspath(
+    os.path.dirname(databench.tests.analyses_broken.__file__))
+
+
 class Build(unittest.TestCase):
-    def file_id(self, filename='databench/tests/analyses/build_test.txt'):
+    def file_id(self, filename=None):
+        if filename is None:
+            filename = os.path.join(ANALYSES_PATH, 'build_test.txt')
         if os.path.exists(filename):
             return '{}'.format(os.path.getmtime(filename))
         return 'file_not_found'
 
     def setUp(self):
-        self.owd = os.getcwd()  # original working directory
+        self.original_working_dir = os.getcwd()  # original working directory
 
     def tearDown(self):
-        os.chdir(self.owd)
+        os.chdir(self.original_working_dir)
 
     def test_build(self):
         before = self.file_id()
 
-        os.chdir('databench')
         subprocess.check_call(['databench', '--build',
-                               '--analyses', 'tests.analyses',
+                               '--analyses', 'databench.tests.analyses',
                                '--coverage', '.coverage'])
-        os.chdir(self.owd)
 
         time.sleep(1)
         after = self.file_id()
@@ -32,10 +40,10 @@ class Build(unittest.TestCase):
     def test_cwd_outside_analyses(self):
         before = self.file_id()
 
-        os.chdir('databench/tests')
+        os.chdir(os.path.join(ANALYSES_PATH, '..'))
         subprocess.check_call(['databench', '--build',
                                '--coverage', '../../.coverage'])
-        os.chdir(self.owd)
+        os.chdir(self.original_working_dir)
 
         time.sleep(1)
         after = self.file_id()
@@ -44,26 +52,23 @@ class Build(unittest.TestCase):
     def test_cwd_inside_analyses(self):
         before = self.file_id()
 
-        os.chdir(os.path.join('databench', 'tests', 'analyses'))
+        os.chdir(ANALYSES_PATH)
         subprocess.check_call(['databench', '--build',
                                '--coverage', '../../../.coverage'])
-        os.chdir(self.owd)
+        os.chdir(self.original_working_dir)
 
         time.sleep(1)
         after = self.file_id()
         self.assertNotEqual(before, after)
 
     def test_broken_analyses(self):
-        before = self.file_id(
-            filename='databench/tests/analyses_broken/build_test.txt')
+        build_file = os.path.join(ANALYSES_BROKEN_PATH, 'build_test.txt')
+        before = self.file_id(build_file)
 
-        os.chdir('databench')
         subprocess.check_call(['databench', '--build',
-                               '--analyses', 'tests.analyses_broken',
+                               '--analyses', 'databench.tests.analyses_broken',
                                '--coverage', '.coverage'])
-        os.chdir(self.owd)
 
         time.sleep(1)
-        after = self.file_id(
-            filename='databench/tests/analyses_broken/build_test.txt')
+        after = self.file_id(build_file)
         self.assertNotEqual(before, after)
