62617dcc596a79f577de187722477a1e34a68a4e
Sven Kreiss <me@svenkreiss.com>
2017-11-10 16:56:11 -0500

    version 0.7b10

--- databench/__init__.py
+++ databench/__init__.py
@@ -3,7 +3,7 @@
 
 from __future__ import absolute_import
 
-__version__ = '0.7b9'
+__version__ = '0.7b10'
 __all__ = ['Analysis', 'AnalysisZMQ', 'App', 'Datastore', 'Meta', 'MetaZMQ',
            'on', 'Readme', 'testing', 'utils']
 
