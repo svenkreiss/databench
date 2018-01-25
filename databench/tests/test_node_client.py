bf9ca62a7f7bb79773075bd921603e950661faad
Sven Kreiss <me@svenkreiss.com>
2018-01-24 20:03:56 -0500

    mocha launches databench

--- databench/tests/test_node_client.py
+++ databench/tests/test_node_client.py
@@ -10,27 +10,6 @@ class NodeClient(unittest.TestCase):
         # transpile js code for node
         subprocess.call(['npm', 'run', 'build'])
 
-        # call os.setsid so that all subprocesses terminate when the
-        # main process receives SIGTERM
-        self.daemon = subprocess.Popen([
-            'databench',
-            '--log', 'WARNING',
-            '--analyses', 'databench.tests.analyses',
-            '--coverage', '.coverage',
-            '--some-test-flag'
-        ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, preexec_fn=os.setsid)
-        time.sleep(5)
-
-    def tearDown(self):
-        # simply DAEMON.terminate() would only terminate the main process,
-        # but the nested processes also need to be terminated
-        if hasattr(signal, 'SIGINT'):
-            os.killpg(self.daemon.pid, signal.SIGINT)
-        else:
-            os.killpg(self.daemon.pid, signal.SIGTERM)
-        time.sleep(5)
-        self.daemon.wait()
-
     def test_node_client(self):
         subprocess.check_call(['npm', 'test'])
 
--- js/src/test.ts
+++ js/src/test.ts
@@ -2,122 +2,161 @@
  * Test documentation.
  */
 
+import * as child_process from 'child_process';
 import { expect } from 'chai';
 import * as Databench from '.';
 
 
-describe('Echo Tests', () => {
-  let c = Databench.connect('ws://localhost:5000/parameters/ws');
-  beforeEach(() => { c.disconnect(); c = Databench.connect('ws://localhost:5000/parameters/ws'); });
-  after(() => c.disconnect());
-
-  it('creates a WebSocket connection', () => {
-    expect(typeof c).to.equal('object');
+describe('Server Process', () => {
+  let databench_process_return_code = -42;
+  const databench_process = child_process.spawn('databench', [
+    '--log', 'WARNING',
+    '--analyses', 'databench.tests.analyses',
+    '--coverage', '.coverage',
+    '--some-test-flag',
+  ]);
+  databench_process.stdout.on('data', data => console.log('databench stdout: ' + data));
+  databench_process.stderr.on('data', data => console.log('databench stderr: ' + data));
+  databench_process.on('exit', code => {
+    databench_process_return_code = code;
+    console.log('databench process exited with code ' + code);
   });
 
-  it('sends an action without message', (done) => {
-    c.on('test_action_ack', () => done());
-    c.emit('test_action');
-  });
-
-  it('echos an object', done => {
-    c.on('test_fn', data => {
-      expect(data).to.deep.equal([1, 2]);
+  before(done => {
+    setTimeout(() => {
+      expect(databench_process_return_code).to.equal(-42);
       done();
-    });
-    c.emit('test_fn', [1, 2]);
+    }, 1000);
   });
 
-  it('echos an empty string', done => {
-    c.on('test_fn', dataEmpty => {
-      expect(dataEmpty).to.deep.equal(['', 100]);
-      done();
-    });
-    c.emit('test_fn', '');
-  });
+  after(() => databench_process.kill());
 
-  it('echos a null parameter', done => {
-    c.on('test_fn', dataNull => {
-      expect(dataNull).to.deep.equal([null, 100]);
-      done();
-    });
-    c.emit('test_fn', null);
-  });
-});
-
-describe('Command Line and Request Arguments', () => {
-  describe('empty request args', () => {
-    const c = new Databench.Connection('ws://localhost:5000/requestargs/ws');
+  describe('Echo Tests', () => {
+    let c = new Databench.Connection();
+    beforeEach(() => { c.disconnect(); c = Databench.connect('ws://localhost:5000/parameters/ws'); });
     after(() => c.disconnect());
 
-    it('valid empty request args', done => {
-      c.on('echo_request_args', request_args => {
-        expect(request_args).to.deep.equal({});
+    it('creates a WebSocket connection', () => {
+      expect(typeof c).to.equal('object');
+    });
+
+    it('sends an action without message', (done) => {
+      c.on('test_action_ack', () => done());
+      c.emit('test_action');
+    });
+
+    it('echos an object', done => {
+      c.on('test_fn', data => {
+        expect(data).to.deep.equal([1, 2]);
         done();
       });
+      c.emit('test_fn', [1, 2]);
+    });
+
+    it('echos an empty string', done => {
+      c.on('test_fn', dataEmpty => {
+        expect(dataEmpty).to.deep.equal(['', 100]);
+        done();
+      });
+      c.emit('test_fn', '');
+    });
+
+    it('echos a null parameter', done => {
+      c.on('test_fn', dataNull => {
+        expect(dataNull).to.deep.equal([null, 100]);
+        done();
+      });
+      c.emit('test_fn', null);
+    });
+  });
+
+  describe('Command Line and Request Arguments', () => {
+    describe('empty request args', () => {
+      const c = new Databench.Connection('ws://localhost:5000/requestargs/ws');
+      after(() => c.disconnect());
+
+      it('valid empty request args', done => {
+        c.on('echo_request_args', request_args => {
+          expect(request_args).to.deep.equal({});
+          done();
+        });
+        c.connect();
+      });
+    });
+
+    describe('simple request args', () => {
+      const c = new Databench.Connection('ws://localhost:5000/requestargs/ws', '?data=requestargtest');
+      after(() => c.disconnect());
+
+      it('can access request args', done => {
+        c.on('echo_request_args', request_args => {
+          expect(request_args).to.deep.equal({ data: ['requestargtest'] });
+          done();
+        });
+        c.connect();
+      });
+    });
+
+    describe('simple cli args', () => {
+      const c = new Databench.Connection('ws://localhost:5000/cliargs/ws');
+      after(() => c.disconnect());
+
+      it('can access cli args', done => {
+        c.on({ data: 'cli_args' }, args => {
+          expect(args).to.deep.equal(['--some-test-flag']);
+          done();
+        });
+        c.connect();
+      });
+    });
+  });
+
+  describe('Cycle Connection', () => {
+    let c = new Databench.Connection();
+    before(() => { c = Databench.connect('ws://localhost:5000/requestargs/ws'); });
+    after(() => c.disconnect());
+
+    it('reconnects after disconnect', done => {
+      c.disconnect();
+      c.on('echo_request_args', () => done());
       c.connect();
     });
   });
 
-  describe('simple request args', () => {
-    const c = new Databench.Connection('ws://localhost:5000/requestargs/ws', '?data=requestargtest');
+  describe('Analysis Test', () => {
+    let c: Databench.Connection = new Databench.Connection();
+    beforeEach(() => { c.disconnect(); c = new Databench.Connection(); });
     after(() => c.disconnect());
 
-    it('can access request args', done => {
-      c.on('echo_request_args', request_args => {
-        expect(request_args).to.deep.equal({ data: ['requestargtest'] });
-        done();
+    it('can open a connection without connecting', done => {
+      c.preEmit('test', message => done());
+      c.emit('test');
+    });
+
+    it('can emulate an empty backend response', done => {
+      c.on('received', message => done());
+      c.preEmit('test', message => c.trigger('received'));
+      c.emit('test');
+    });
+
+    it('can emulate a string backend response', () => {
+      c.on('received', message => {
+        expect(message).to.equal('test message');
       });
-      c.connect();
+      c.preEmit('test', message => c.trigger('received', 'test message'));
+      c.emit('test');
     });
   });
 
-  describe('simple cli args', () => {
-    const c = new Databench.Connection('ws://localhost:5000/cliargs/ws');
-    after(() => c.disconnect());
+  describe('Parameter Tests', () => {
+    let databench = new Databench.Connection();
+    beforeEach(() => { databench.disconnect(); databench = Databench.connect('ws://localhost:5000/parameters/ws'); });
+    after(() => databench.disconnect());
 
-    it('can access cli args', done => {
-      c.on({ data: 'cli_args' }, args => {
-        expect(args).to.deep.equal(['--some-test-flag']);
-        done();
-      });
-      c.connect();
+    it('calls an action without parameter', done => {
+      databench.on('test_action_ack', () => done());
+      databench.emit('test_action');
     });
   });
-});
 
-describe('Cycle Connection', () => {
-  const c = Databench.connect('ws://localhost:5000/requestargs/ws');
-  after(() => c.disconnect());
-
-  it('reconnects after disconnect', done => {
-    c.disconnect();
-    c.on('echo_request_args', () => done());
-    c.connect();
-  });
-});
-
-describe('Analysis Test', () => {
-  let c: Databench.Connection = new Databench.Connection();
-  beforeEach(() => { c.disconnect(); c = new Databench.Connection(); });
-  after(() => c.disconnect());
-
-  it('can open a connection without connecting', done => {
-    c.preEmit('test', message => done());
-    c.emit('test');
-  });
-
-  it('can emulate an empty backend response', done => {
-    c.on('received', message => done());
-    c.preEmit('test', message => c.trigger('received'));
-    c.emit('test');
-  });
-
-  it('can emulate a string backend response', () => {
-    c.on('received', message => {
-      expect(message).to.equal('test message');
-    });
-    c.preEmit('test', message => c.trigger('received', 'test message'));
-    c.emit('test');
-  });
 });
