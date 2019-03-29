d950cfee7b89729b880a425348293dc774f2aa15
Sven Kreiss <me@svenkreiss.com>
2019-03-29 12:59:51 +0100

    debug print statements

--- databench/template.py
+++ databench/template.py
@@ -13,17 +13,23 @@ class Loader(tornado.template.BaseLoader):
                       for root_directory in root_directories]
 
     def resolve_path(self, name, parent_path=None):
+        print(name, parent_path)
         for root in self.roots:
-            if parent_path and not parent_path.startswith('<') and \
-                not parent_path.startswith('/') and \
-                    not name.startswith('/'):
+            print('root', root)
+            if parent_path and \
+               not parent_path.startswith('<') and \
+               not parent_path.startswith('/') and \
+               not name.startswith('/'):
                 root = os.path.join(root, parent_path)
             path = os.path.join(root, name)
             if os.path.exists(path):
+                print('path', path)
                 return path
+        print('name', name)
         return name
 
     def _create_template(self, name):
+        print('before open', name)
         with open(name, 'rb') as f:
             template = tornado.template.Template(
                 f.read(), name=name, loader=self)
