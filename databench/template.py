ad6366377f107e86f97854713e1b798abc7ddbe6
Sven Kreiss <me@svenkreiss.com>
2019-03-29 16:53:33 +0100

    cleanup

--- databench/template.py
+++ databench/template.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import tornado.template
 
@@ -14,25 +13,21 @@ class Loader(tornado.template.BaseLoader):
                       for root_directory in root_directories]
 
     def resolve_path(self, name, parent_path=None):
-        logging.error('name %s, p %s', name, parent_path)
         for root in self.roots:
-            logging.error('root %s', root)
             if parent_path and \
                not parent_path.startswith('<') and \
                not parent_path.startswith('/') and \
-               not name.startswith('/') and \
-               parent_path[1:3] != ':\\':
-                logging.error('merging %s %s %s', root, parent_path, name)
+               parent_path[1:3] != ':\\' and \
+               not name.startswith('/'):
                 root = os.path.join(root, parent_path)
+
             path = os.path.join(root, name)
             if os.path.exists(path):
-                logging.error('path %s', path)
                 return path
-        logging.error('name %s', name)
+
         return name
 
     def _create_template(self, name):
-        logging.error('before open %s', name)
         with open(name, 'rb') as f:
             template = tornado.template.Template(
                 f.read(), name=name, loader=self)
